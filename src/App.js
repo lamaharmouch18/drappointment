31fb32073d055e20f61e93ffb291ec7e15bdb85a
Rehamaasar <52230205@students.liu.edu.lb>
2025-11-15 16:52:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,107 @@
-<<<<<<< Updated upstream
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// REHAM & LAMA - Main application file
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Layout (wraps pages with Navbar + Footer)
+import Layout from "./Layout";
+
+// Pages
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Services from "./pages/Services";
 import Doctors from "./pages/Doctors";
-import Login from "./pages/Login";
+import DoctorDetail from "./pages/DoctorDetail";
 import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/doctors" element={<Doctors />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        {/* Home page */}
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+
+        {/* About page */}
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <About />
+            </Layout>
+          }
+        />
+
+        {/* Services page */}
+        <Route
+          path="/services"
+          element={
+            <Layout>
+              <Services />
+            </Layout>
+          }
+        />
+
+        {/* All doctors list */}
+        <Route
+          path="/doctors"
+          element={
+            <Layout>
+              <Doctors />
+            </Layout>
+          }
+        />
+
+        {/* Single doctor details page, ex: /doctors/1 */}
+        <Route
+          path="/doctors/:id"
+          element={
+            <Layout>
+              <DoctorDetail />
+            </Layout>
+          }
+        />
+
+        {/* Contact / appointment page */}
+        <Route
+          path="/contact"
+          element={
+            <Layout>
+              <Contact />
+            </Layout>
+          }
+        />
+
+        {/* Login page (no need for layout if you want it full screen) */}
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+
+        {/* 404 - page not found */}
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <NotFound />
+            </Layout>
+          }
+        />
       </Routes>
-      <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
 
 export default App;
-=======
->>>>>>> Stashed changes
