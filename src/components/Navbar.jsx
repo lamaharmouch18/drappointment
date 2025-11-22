878026f22f30bb1fd888c5259c624dcc27a0a46e
Rehamaasar <52230205@students.liu.edu.lb>
2025-11-22 21:59:28 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-// REHAM
 
-import { Link } from "react-router-dom";
-
-function Navbar() {
-  return (
-    <header className="p-4 bg-white shadow">
-      <nav className="flex justify-between items-center container mx-auto">
-        <Link to="/" className="text-2xl font-bold text-blue-600">
-          DrAppointment
-        </Link>
-
-        <div className="flex gap-6">
-          <Link to="/">Home</Link>
-          <Link to="/services">Services</Link>
-          <Link to="/doctors">Doctors</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/login">Login</Link>
-        </div>
-      </nav>
-    </header>
-  );
-}
-
-export default Navbar;
-=======
 import { NavLink, useNavigate } from "react-router-dom";
 import { HeartPulse } from "lucide-react";
 
@@ -100,4 +74,4 @@ export default function Navbar() {
     </header>
   );
 }
->>>>>>> 5406e0bb834f0bfc5c07bd7da41f12f5a7d5e74a
+
